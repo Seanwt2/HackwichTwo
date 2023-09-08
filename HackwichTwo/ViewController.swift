5078ecbf3b6a7f9ceb850d5bd6d02359fe637161
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-09-08 13:25:23 -1000

    Completed Parts 8

--- HackwichTwo/ViewController.swift
+++ HackwichTwo/ViewController.swift
@@ -8,14 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//part 8
+    var string1 = "Hello"
+    var string2 = " there"
+    var string3 = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        string3 = string1 + string2
+        
     }
 
 
     @IBAction func pressMeButtonPressed(_ sender: Any) {print("hello World")
+    }
+    
+    @IBAction func IBAactionButtonPressed(_ sender: Any) {print(string3)
     }
 }
 
