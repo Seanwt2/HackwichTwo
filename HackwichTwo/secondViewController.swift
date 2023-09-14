0da417104edd519e5e229dc5d320d0fe1e7c1a00
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-09-13 14:45:07 -1000

    Completed Parts 1-7

--- HackwichTwo/secondViewController.swift
+++ HackwichTwo/secondViewController.swift
@@ -8,28 +8,38 @@
 import UIKit
 
 class secondViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBAction func ChangeColorButtonPressed(_ sender: Any) {
     
-    //turning blue
-        self.view.backgroundColor = UIColor.blue
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBAction func ChangeColorButtonPressed(_ sender: Any) {
+//button press block
+        
+        //turning blue
+    self.view.backgroundColor = UIColor.blue
+        //label stuff
+        
+        firstlabel.self.text = "Hello World"
+        firstlabel.self.textColor = UIColor.red
         
     }
+    
+    
+    @IBOutlet weak var firstlabel: UILabel!
+    
+    
 }
